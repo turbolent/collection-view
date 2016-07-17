10f43670f83039ab0b51b558629495bc42eb9897
Bastian Müller <bastian@turbolent.com>
2016-07-17 11:20:27 -0700

    add example for starting empty and loading a few items

---
+++ examples/load-few/src/index.js
@@ -0,0 +1,32 @@
+
+import { CollectionView, GridLayout } from '../../../src'
+import style from '../../_common/style.css'
+
+
+class Delegate {
+
+  constructor(items) {
+    this.items = items
+  }
+
+  getCount() {
+    return this.items.length
+  }
+
+  configureElement(element, index) {
+    element.classList.add(style.box)
+    element.textContent = this.items[index]
+  }
+}
+
+window.onload = function () {
+  const delegate = new Delegate([])
+  const scrollElement = document.getElementById('scroll')
+  const layout = new GridLayout()
+  const view = new CollectionView(scrollElement, layout, delegate)
+  const button = document.getElementById('button')
+  button.onclick = function () {
+    delegate.items = ["A", "B", "C"]
+    view.changeIndices([], delegate.items.map((_, index) => index), {})
+  }
+}
