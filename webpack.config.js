3d0b7d7fd3d62efd7a10d871008546f1447970ae
Bastian Müller <bastian@turbolent.com>
2017-11-05 12:09:05 -0800

    minify when building for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
 const path = require('path')
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 
-module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
+module.exports = (env) => {
+  let plugins = []
+  if (env === 'production')
+    plugins = [new MinifyPlugin()];
+
+  return {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              }
             }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ ".ts", ".js" ]
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: "umd"
-  },
-  plugins: [
-  ]
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [ ".ts", ".js" ]
+    },
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: "umd"
+    },
+    plugins
+  }
 };
